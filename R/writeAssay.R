33964cb8796fde2f11e2145241b9aefe84958e44
James Ashmore <jma1991@icloud.com>
2019-06-16 11:39:47 +0100

    Commit message

--- R/writeAssay.R
+++ R/writeAssay.R
@@ -3,10 +3,11 @@
 #' Export assay values over ranges to a bigWig file format.
 #'
 #' @param object A \code{RangedSummarizedExperiment} object.
+#' @param assay Name of assay slot.
 #' @param file A character string naming a file open for writing.
 #' @param sample Either a column name or index.
 
-writeAssay <- function(object, file = "", sample = NULL) {
+writeAssay <- function(object, file = "", assay = "", sample = "") {
 
     # Check argument missing
     if (missing(object)) {
@@ -14,6 +15,9 @@ writeAssay <- function(object, file = "", sample = NULL) {
     }
     if (missing(file)) {
         stop("`file` is missing, with no default.", call. = FALSE)
+    }
+    if (missing(assay)) {
+        stop("`assay` is missing, with no default.", call. = FALSE)
     }
     if (missing(sample)) {
         stop("`sample` is missing, with no default.", call. = FALSE)
@@ -26,32 +30,67 @@ writeAssay <- function(object, file = "", sample = NULL) {
     if (!is(file, "character")) {
         stop("`file` must be a single character string.", call. = FALSE)
     }
+    if (!is(assay, "character")) {
+        stop("`assay` must be a single character string.", call. = FALSE)
+    }
     if (!(class(sample) %in% c("character", "numeric"))) {
         stop("`sample` must be a numeric or character class.", call. = FALSE)
     }
 
     # Check argument value
+    if (!(assay %in% assayNames(object))) {
+        stop("Assay '", assay, "' not found.", call. = FALSE)
+    }
     if (!(sample %in% c(seq_len(ncol(object)), colnames(object)))) {
         stop("Sample '", sample, "' not found.", call. = FALSE)
     }
 
     # Dispatch relevant method
-    UseMethod("writeAssay", object)
+    if (assay == "countsData") {
+        writeAssay.counts(object, file, sample)
+    } else if (assay == "qsmoothData") {
+        writeAssay.qsmooth(object, file, sample)
+    } else {
+        stop("Error!")
+    }
 
 }
 
-writeAssay.RangedSummarizedExperiment <- function(object, file = "", sample = NULL) {
+writeAssay.counts <- function(object, file = "", sample = NULL) {
 
-    assayNames <- assayNames(object)
+    # Extract assay and range data
+    assayData <- assay(object, "countsData")
+    rangeData <- rowRanges(object)
 
-    rowRanges <- rowRanges(object)
+    # Filter assay data to chosen sample
+    colIndex <- sample
+    colAssay <- assayData[, colIndex]
 
-    if (assayNames[1] == "countsData") {
-        score(rowRanges) <- assay(object)[, sample]
-    } else if (assayNames[1] == "qsmoothData") {
-        score(rowRanges) <- 2^assay(object)[, sample]
-    }
+    # Assign assay to score column
+    score(rangeData) <- colAssay
 
     # Export coverage track
     rtracklayer::export(rowRanges, con = file, format = "bigWig")
+
+}
+
+writeAssay.qsmooth <- function(object, file = "", sample = NULL) {
+
+    # Extract assay and range data
+    assayData <- assay(object, "qsmoothData")
+    rangeData <- rowRanges(object)
+
+    # Filter assay data to chosen sample
+    colIndex <- sample
+    colAssay <- assayData[, colIndex]
+
+    # Reverse log transformation from qsmooth
+    colAssay <- 2^colAssay
+
+    # Assign assay to score column
+    score(rangeData) <- colAssay
+
+    # Export coverage track
+    rtracklayer::export(rowRanges, con = file, format = "bigWig")
+
 }
